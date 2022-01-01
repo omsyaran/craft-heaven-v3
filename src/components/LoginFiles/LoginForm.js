6ff0e197818ae270af6d7acb02b27b9a1de33bc2
omsyaran <omsyaranc24@gmail.com>
2022-01-02 01:03:49 +0800

    halfway/logincss

--- src/components/LoginFiles/LoginForm.js
+++ src/components/LoginFiles/LoginForm.js
@@ -3,7 +3,11 @@ import { auth } from "/Users/omsyaran/Desktop/craft-heaven-v3/src/firebase-confi
 import { useState } from "react";
 import { onAuthStateChanged } from "firebase/auth";
 import { signOut } from "firebase/auth";
+import './LoginFormStyle.css';
 import weblogo from './logo-icon.svg';
+import candlepic from './candle.jpg';
+import emaillogo from './user.svg';
+import passkey from './pass-key.svg';
 
 function LoginForm(){
 
@@ -31,20 +35,24 @@ function LoginForm(){
     return(
         <div className="body">
             <img className='logo' src={weblogo} alt="CraftHeaven logo"/>
-            <div className="form-inner">
-                <div className="form-group">
-                    <input size="47" classname='inputs' type="text" name="login-email" id="login-email" placeholder="Email" onChange={(event) => {setLoginEmail(event.target.value);}}/>
+            <img className='candles' src={candlepic} alt="Candlepic"/>
+            <div className="form">
+                <div className="form-inner">
+                    <div className="form-group">
+                        <img className='user-logo' src={emaillogo} alt="Email logo"/>
+                        <input size="47" classname='inputs' type="text" name="login-email" id="login-email" placeholder="Email" onChange={(event) => {setLoginEmail(event.target.value);}}/>
+                    </div>
+                    <br></br>
+                    <div className="form-group">
+                        <img className='passkey-logo' src={passkey} alt="Password logo"/>
+                        <input size="47" className="inputs" type="password" name="login-password" id="login-password" placeholder="Password"  onChange={(event) => {setLoginPassword(event.target.value);}}/>
+                    </div>
+                    <br></br>
+                    <button onClick={login}>Login</button>
+                    <button onClick={logout}>Sign out</button>
+                    <h6 className="register-opt">User registered in : </h6>{user?.email}
                 </div>
-                <br></br>
-                <div className="form-group">
-                    <input size="47" className="inputs" type="password" name="login-password" id="login-password" placeholder="Password"  onChange={(event) => {setLoginPassword(event.target.value);}}/>
-                </div>
-                <br></br>
-                <button onClick={login}>Login</button>
-                <button onClick={logout}>Sign out</button>
             </div>
-            <h4>User registered in : </h4>
-            {user?.email}
         </div>
     );
 }
