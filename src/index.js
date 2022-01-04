7ea0c6441f2c2095e9d94bb64a41ea1b285a90d4
omsyaran <omsyaranc24@gmail.com>
2022-01-05 00:30:27 +0800

    flow is working!!!

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import LoginForm from './components/LoginFiles/LoginForm';
-import RegisterForm from './components/RegisterFiles/RegisterForm';
+import App from './App';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <RegisterForm />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -16,5 +15,3 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
-// <LoginForm />
