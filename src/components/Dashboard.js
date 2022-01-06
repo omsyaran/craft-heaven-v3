7f9690bc08d5f7e7f6f6275a44ee7cd40c6d4437
omsyaran <omsyaranc24@gmail.com>
2022-01-07 03:48:58 +0800

    minor changes

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -22,20 +22,9 @@ const Dashboard = ({ history }) => {
   const user = auth.currentUser;
 
   return (
-    <div className="w-full h-screen bg-gradient-to-r from-yellow-200 via-red-500 to-pink-500 flex justify-center items-center">
-      <div className="w-96 bg-white shadow-lg">
-        <div className="m-5">
-          <p>{user && user.displayName}</p>
-        </div>
-        <div className="m-5">
-          <button
-            onClick={logout}
-            className="bg-gradient-to-r from-pink-500 via-red-500 to-yellow-200 text-white px-10 py-2 rounded text-xl font-bold"
-          >
-            Logout
-          </button>
-        </div>
-      </div>
+    <div>
+      <p>{user && user.displayName}</p>
+      <button onClick={logout}>Logout</button>
     </div>
   );
 };
