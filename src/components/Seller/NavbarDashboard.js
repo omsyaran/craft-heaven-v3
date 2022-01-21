24a5fd56e3851c32245d855deda997d189f50d79
omsyaran <omsyaranc24@gmail.com>
2022-01-21 18:54:34 +0800

    seller dashboard is working fully

--- src/components/Seller/NavbarDashboard.js
+++ src/components/Seller/NavbarDashboard.js
@@ -1,39 +1,60 @@
 import React from "react";
-import sellerProfile from "../Seller/sellerProfile.png";
 import { getAuth } from "firebase/auth";
 import { getDatabase, ref, onValue } from "firebase/database";
 import { initilizeFirebase } from "../SignUp/firebase";
+import { useState } from "react";
 
 const NavbarDashboard = () => {
   const app = initilizeFirebase();
   const db = getDatabase(app);
   const auth = getAuth(app);
   const SellerID = auth.currentUser.uid;
+  const [Pic, setPic] = useState(null);
+  const [Date, setDate] = useState("");
+  const [StoreName, setStoreName] = useState("");
 
   const getStorePic = ref(db, "Sellers/" + SellerID + "/store_pic");
-  onValue(getStorePic, (snapshot) => {
-    const StorePic = snapshot.val();
-    dummy_function1(StorePic);
-  });
+  onValue(
+    getStorePic,
+    (snapshot) => {
+      const StorePic = snapshot.val();
+      setPic(StorePic);
+    },
+    {
+      onlyOnce: true,
+    }
+  );
   const getStoreDate = ref(db, "Sellers/" + SellerID + "/store_established");
-  onValue(getStoreDate, (snapshot) => {
-    const StoreDate = snapshot.val();
-    dummy_function2(StoreDate);
-  });
-
-  function dummy_function1(StorePic) {
-    alert(StorePic);
-  }
-
-  function dummy_function2(StoreDate) {
-    alert(StoreDate);
-  }
+  onValue(
+    getStoreDate,
+    (snapshot) => {
+      const StoreDate = snapshot.val();
+      setDate(StoreDate);
+    },
+    {
+      onlyOnce: true,
+    }
+  );
+  const getStoreName = ref(db, "Sellers/" + SellerID + "/store_name");
+  onValue(
+    getStoreName,
+    (snapshot) => {
+      const StoreName = snapshot.val();
+      setStoreName(StoreName);
+    },
+    {
+      onlyOnce: true,
+    }
+  );
 
   return (
     <div>
-      <img src={sellerProfile} width={139} />
-      <p>Sivali Factory</p>
-      <p>Joined on : 20 Feb 2020</p>
+      <img
+        src={Pic !== null ? Pic : require("../Seller/sellerProfile.png")}
+        width={139}
+      />
+      <p>{StoreName}</p>
+      <p>Joined on : {Date}</p>
     </div>
   );
 };
