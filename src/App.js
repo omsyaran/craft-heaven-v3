dd2c7154b4ed99613d3b31bdbf8215488015ad42
omsyaran <omsyaranc24@gmail.com>
2022-01-21 16:17:54 +0800

    setup for dashboard env

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Signup from "./components/SignUp/Signup";
 import SellerLogin from "./components/SellerLogIn/SellerLogIn";
 import SellerSignup from "./components/SellerSignUp/SellerSignup";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import NavbarDashboard from "./components/Seller/NavbarDashboard";
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={SellerSignup} />
-          <Route path="/login" component={SellerLogin} />
+          <Route path="/login" component={NavbarDashboard} />
         </Switch>
       </BrowserRouter>
     </div>
